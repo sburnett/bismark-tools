c825479f8efcacd04a62201c4f7e5572d14e60ba
Sam Burnett <srburnet@gmail.com>
2013-06-12 18:01:12 +0000

    Tweak info display

--- bdm-client/commands/info.go
+++ bdm-client/commands/info.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/bmizerany/pq"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -25,34 +26,36 @@ func (name Info) Run(args []string) error {
 	}
 	defer db.Close()
 
+	var pattern string
+	if len(args) > 0 {
+		pattern = args[0]
+	}
 	queryString := `
         SELECT
             id AS node,
             ip,
             bversion AS version,
-            date_trunc('second', date_last_seen) AS last_probe,
-            extract(epoch from date_trunc('second', date_last_seen + '60 seconds' - current_timestamp)) AS next_probe,
-            date_trunc('second', age(current_timestamp, date_last_seen)) AS outage_duration
+            date_trunc('second', date_last_seen),
+            date_trunc('second', age(current_timestamp, date_last_seen)),
+            extract(epoch from date_trunc('second', current_timestamp - date_last_seen))
         FROM devices
         WHERE id LIKE $1`
-	rows, err := db.Query(queryString, "%"+args[0])
+	rows, err := db.Query(queryString, "%"+pattern)
 	if err != nil {
 		return fmt.Errorf("Error querying devices table: %s", err)
 	}
 
 	var nodeId, ipAddress string
-	var version int
+	var version string
 	var lastSeen time.Time
-	var nextPingSeconds float64
 	var outageDuration string
+	var outageSeconds float64
+	var nodeIds []string
 	rowCount := 0
 	for rows.Next() {
 		rowCount++
-		if rowCount > 1 {
-			fmt.Fprintln(os.Stderr, "That device ID is ambiguous")
-			os.Exit(1)
-		}
-		rows.Scan(&nodeId, &ipAddress, &version, &lastSeen, &nextPingSeconds, &outageDuration)
+		rows.Scan(&nodeId, &ipAddress, &version, &lastSeen, &outageDuration, &outageSeconds)
+		nodeIds = append(nodeIds, nodeId)
 	}
 	if err := rows.Err(); err != nil {
 		return fmt.Errorf("Error iterating through devices table: %s", err)
@@ -60,18 +63,23 @@ func (name Info) Run(args []string) error {
 	if rowCount == 0 {
 		fmt.Fprintf(os.Stderr, "Device %s doesn't exist\n", args[0])
 		os.Exit(1)
+	} else if rowCount > 1 {
+		fmt.Fprintln(os.Stderr, "That device ID is ambiguous:", strings.Join(nodeIds, ", "))
+		os.Exit(1)
 	}
 
-	switch {
-	case lastSeen.IsZero():
-		fmt.Printf("Device %s has never sent any probes.\n", nodeId, outageDuration, lastSeen)
-	case nextPingSeconds >= 0:
-		fmt.Printf("Device %s is online and should be sending another probe in about %s.\n", nodeId, (time.Second * time.Duration(nextPingSeconds)))
-	case nextPingSeconds < -600:
-		fmt.Printf("Device %s has been offline for %s, since %s.\n", nodeId, outageDuration, lastSeen)
-	default:
-		fmt.Printf("Device %s is about %s late sending its next probe.\n", nodeId, (time.Second * time.Duration(-1*nextPingSeconds)))
+	deviceStatus := OutageDurationToDeviceStatus(outageSeconds)
+	nextPing := OutageDurationToNextProbe(outageSeconds)
+	switch deviceStatus {
+	case Online:
+		fmt.Printf("%s is online and should be sending its next probe in about %s.\n", nodeId, secondsToDurationString(nextPing))
+	case Stale:
+		fmt.Printf("%s is about %s late sending its next probe.\n", nodeId, secondsToDurationString(-1*nextPing))
+	case Offline:
+		fmt.Printf("%s has been offline for %s, since %s.\n", nodeId, outageDuration, lastSeen)
 	}
+
+	fmt.Printf("Its public IP address is %s and its firmware version is %s.\n", ipAddress, version)
 
 	return nil
 }
