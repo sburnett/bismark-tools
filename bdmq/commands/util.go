479382a291709af33bf26dd60c1790a6f28a2b5b
Sam Burnett <srburnet@gmail.com>
2013-06-13 22:10:10 +0000

    Remove unused function

--- bdmq/commands/util.go
+++ bdmq/commands/util.go
@@ -11,7 +11,3 @@ func fprintWithTabs(writer io.Writer, values ...interface{}) (int, error) {
 	formatString := strings.Repeat("%v\t", len(values))
 	return fmt.Fprintf(writer, strings.TrimRight(formatString, "\t")+"\n", values...)
 }
-
-func secondsToDurationString(seconds float64) string {
-	return (time.Second * time.Duration(seconds)).String()
-}
